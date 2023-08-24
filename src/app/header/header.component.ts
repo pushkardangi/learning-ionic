9ed665b31f930d0d697c3b6bf3b2a89efdb758f0
Pushkar Dangi <pushkardangi746@gmail.com>
2023-08-24 21:39:15 +0530

    ui: login and signup linked with header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {}
-
 }
